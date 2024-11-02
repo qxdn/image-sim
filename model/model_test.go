db78cf63a899fd4db3801582b64655592d09a13e
Xu Qian <1464238196@qq.com>
2024-11-02 11:25:28 +0800

    add model test

---
+++ model/model_test.go
@@ -0,0 +1,33 @@
+package model
+
+import (
+	"errors"
+	"fmt"
+	"testing"
+
+	"github.com/qxdn/imagesim/util"
+)
+
+func TestHash(t *testing.T) {
+	// 读图
+	demo1, err1 := util.ReadImage("../demo/demo1.jpg")
+	demo2, err2 := util.ReadImage("../demo/demo2.jpg")
+	if err := errors.Join(err1, err2); err != nil {
+		t.Errorf("ReadImage error:%v", err)
+		return
+	}
+	// 算hash
+	hash1, err1 := ComputeHashes(&demo1)
+	hash2, err2 := ComputeHashes(&demo2)
+	if err := errors.Join(err1, err2); err != nil {
+		t.Errorf("ComputeHashes error:%v", err)
+		return
+	}
+
+	fmt.Printf("demo1: AHash:%v,DHash:%v,PHash:%v\n", hash1.AHash, hash1.DHash, hash1.PHash)
+	fmt.Printf("demo2: AHash:%v,DHash:%v,PHash:%v\n", hash2.AHash, hash2.DHash, hash2.PHash)
+	// 比较hash
+	fmt.Println("AHash Distance:", util.HashDistance(hash1.AHash, hash2.AHash))
+	fmt.Println("DHash Distance:", util.HashDistance(hash1.DHash, hash2.DHash))
+	fmt.Println("PHash Distance:", util.HashDistance(hash1.PHash, hash2.PHash))
+}
